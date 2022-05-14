60323069731edf007ebee39bd53592f5fa38eb62
thrivediraju <104252421+thrivediraju@users.noreply.github.com>
2022-05-14 21:59:42 +0530

    Add files via upload

---
+++ Data_types.py
@@ -0,0 +1,54 @@
+''' About Integer Data Type  '''
+# print(type(6))
+# print(type(2))
+# print(type(0))
+# print(type(-7))
+
+# a=6
+# print(a)
+# print('%d' %a)
+# print('%i' %a)
+
+#-------------------------------------
+
+"""" Float Data Type"""
+# print(type(0.0))
+# print(type(2.3))
+# print(type(-6.7))
+# print(type(.3))
+
+# b=4.7 
+# print(b)
+# print('%f' %b) 
+# print('%.1f' %b)
+# print('%.2f' %b)
+# print('%.3f' %b)
+# print('%3f' %b)
+# print('%8f' %b)
+# print('%9f' %b)
+# print('%12f' %b)
+
+# print(type('hi')) 
+# print(type('8')) 
+# print(type('4.7')) 
+
+# a='hi' ; b="bye" ; c='''chai'''; d="""die"""
+# print(a,type(a))
+# print(b,type(b))
+# print(c,type(c))
+# print(d,type(d))
+
+# print('%s'%b)
+# print('%s'%a)
+# print('%s'%c)
+
+# print(type(True))
+# print(type(False))
+# print(6<7) # True
+# print(4>7) # False
+# print(7==7) # True
+# print(7!=7) # False
+
+# print('hey'.isidentifier())
+# print('_hey'.isidentifier())
+# print('5hey'.isidentifier())
